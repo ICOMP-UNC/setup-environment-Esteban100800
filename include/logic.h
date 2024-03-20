26a07222697c0187e4b0005510ce6491ab82ea37
Esteban100800 <esteban.moroz@mi.unc.edu.ar>
2024-03-20 20:07:35 -0300

    CL: Actions
    UT, Coverage and Actions

---
+++ include/logic.h
@@ -0,0 +1,45 @@
+#pragma once
+
+/**
+ * 
+ * @brief add two numbers
+ * 
+ * @param first_number
+ * @param second_number
+ * @return sum of two numbers
+*/
+
+int add (int first_number, int second_number);  
+
+/**
+ * 
+ * @brief subtract two numbers
+ * 
+ * @param first_number
+ * @param second_number
+ * @return difference of two numbers
+*/
+
+int subtract (int first_number, int second_number);
+
+/**
+ * 
+ * @brief multiply two numbers
+ * 
+ * @param first_number
+ * @param second_number
+ * @return product of two numbers
+*/  
+
+int multiply (int first_number, int second_number);
+
+/**
+ * 
+ * @brief divide two numbers
+ * 
+ * @param first_number
+ * @param second_number
+ * @return result of division
+*/
+
+double divide (int first_number, int second_number);
